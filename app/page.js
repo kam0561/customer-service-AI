7ee42ca23194f0acc6062452263f28fff80caf6a
Kunwar <kunwarabhay05@gmail.com>
2024-08-11 12:59:08 +0530

    error resolved

--- app/page.js
+++ app/page.js
@@ -16,49 +16,48 @@ export default function Home() {
 
   const [message, setMessage]= useState('')
 
-  const sendMessage = async()=> {
-    setMessage('')
-    setMessages(('messages') => [
+  const sendMessage = async () => {
+    setMessage('');
+    setMessages((messages) => [
       ...messages,
-      {role:'user', content:message},
-      {role:'assistant', content:''},
-    ])
-  }
-
-  const response = fetch('/api/chat', {
-    method:'POST',
-    headers: {
-      'Content-Type':'application-json',
-    },
-    body: JSON.stringify([...messages,{role:'user', content:'message'}]),
-  }).then(async (res) => {
-    const reader= res.body.getReader()
-    const decoder= new TextDecoder()
-
-    let result=''
-    return reader.read().then( function processText({done,value}){
-        if (done){
+      { role: 'user', content: message },
+      { role: 'assistant', content: '' },
+    ]);
+  
+    const response = await fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([...messages, { role: 'user', content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+  
+      let result = '';
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
           return result;
         }
-
-    const text= decoder.decode(value|| new Int8Array, {stream:true})
-    setMessages((messages) =>{
-      let lastMessage= messages[messages.length-1]
-      let otherMessages= messages.slice[0, messages.length -1]
-
-      return [
-        ...otherMessages,
-        {
-          ...lastMessage,
-          content: lastMessage.content + text,
-        },
-      ]
-    })
-      return reader.read().then(processText)
-  })
-  })
-  }
-
+  
+        const text = decoder.decode(value || new Int8Array(), { stream: true });
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1];
+          let otherMessages = messages.slice(0, messages.length - 1);
+  
+          return [
+            ...otherMessages,
+            {
+              ...lastMessage,
+              content: lastMessage.content + text,
+            },
+          ];
+        });
+        return reader.read().then(processText);
+      });
+    });
+  };
+  
   return <Box
         width='100vw'
         height='100vh'
