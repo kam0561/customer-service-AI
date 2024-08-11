06be74da99abb3f6e095e52d95c3e3693fb92a4d
Kunwar <kunwarabhay05@gmail.com>
2024-08-11 12:24:53 +0530

    textfield and button added

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import { Assistant } from "next/font/google";
+import { Button, TextField } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 
 export default function Home() {
@@ -24,29 +26,63 @@ export default function Home() {
         bgcolor={'#0f0f0f'}>
 
         <Stack
+        width={'1000px'}
+        height={'700px'}
+        border={'1px solid white'}
         direction={'column'}
         spacing={2}
-        flexGrow={1}
-        maxHeight={'100%'}
-        overflow={'auto'}
+        p={2}
         >
+          <Stack
+          direction={'column'}
+          spacing={2}
+          flexGrow={1}
+          maxHeight={'100%'}
+          overflow={'auto'}>
         {messages.map((message,index) =>(
           <Box
           key={'index'}
           display={'flex'}
           justifyContent={
-            message.role === 'assistent'? 'flex-start':'flex-end'
+            message.role === 'assistant'? 'flex-start':'flex-end'
           }>
             <Box
-            bgcolor={message.role != 'assistent'? '#D3D3D3': '#f0f0f0'}
-            color={'white'}
+            bgcolor={message.role != 'assistant'? '#D3D3D3': '#f0f0f0'}
+            color={'black'}
             borderRadius={16}
             p={3}>
-              
+              {message.content}
             </Box>
           </Box>
           ))}
+          </Stack>
+          <Stack direction={'row'} spacing={2} >
+            <TextField 
+            label={'Message'}
+            variant="filled"
+            fullWidth
+            value={'Message'}
+            onChange={(e)=> setMessage(e.target.value)
+            }
+            sx={{
+              '& .MuiInputBase-root': {
+                backgroundColor: 'white',
+              },
+              '& .MuiInputBase-root.Mui-focused': {
+                backgroundColor: 'white',
+              },
+              '& .MuiInputBase-input': {
+                color: 'black',
+              },
+              '& .MuiInputBase-root:hover': {
+                backgroundColor: 'white',
+              },
+            }}
+            />
+            <Button variant="contained" color="success" endIcon={<SendIcon />}>
+              Send
+            </Button>
+          </Stack>
         </Stack>
-    
         </Box>
 }
